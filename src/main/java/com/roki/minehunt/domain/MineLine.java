5b17c78dc91c61b6b544598d8161af565bb7e4fc
김경록 <goodboy3421@gmail.com>
2020-07-15 21:33:41 +0900

    feat: MineLine 기능 추가
    MineBoard의 행 역할을 담당하는 (Square)의 일급 컬렉션

---
+++ src/main/java/com/roki/minehunt/domain/MineLine.java
@@ -0,0 +1,26 @@
+package com.roki.minehunt.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MineLine {
+    public static final int MINELINE_LENGTH = 10; //todo: MineLine를 생성하는 전략에서 쓰인다?
+    private static final String MINELINE_LENGTH_MESSAGE = "지뢰판의 가로길이는 10이어야 합니다.";
+
+    private final List<Square> squares;
+
+    public MineLine(List<Square> squares) {
+        validateMines(squares);
+        this.squares = new ArrayList<>(squares);
+    }
+
+    private void validateMines(List<Square> mines) {
+        if (mines.size() != MINELINE_LENGTH) {
+            throw new IllegalArgumentException(MINELINE_LENGTH_MESSAGE);
+        }
+    }
+
+    public List<Square> getSquares() {
+        return new ArrayList<>(squares);
+    }
+}
