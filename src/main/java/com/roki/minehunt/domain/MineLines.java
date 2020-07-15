fc2bd18a8a4d89c7794e7d40d4e8efda0e2a35f0
김경록 <goodboy3421@gmail.com>
2020-07-16 00:15:41 +0900

    feat: MineLines 기능 추가
    MineLine(지뢰창의 열 역할)의 일급 컬렉션 MineLines(지뢰창의 행 역할)

---
+++ src/main/java/com/roki/minehunt/domain/MineLines.java
@@ -0,0 +1,39 @@
+package com.roki.minehunt.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MineLines {
+    public static final int MINELINES_LENGTH = 10;
+    private static final String MINELINES_LENGTH_MESSAGE = "지뢰판의 세로길이는 10이어야 합니다.";
+    public static final int MINE_NUMBER_LIMIT = 10;
+
+    private final List<MineLine> mineLines;
+
+    public MineLines(MineLineGenerator mineLineGenerator) {
+        List<MineLine> mineLines = new ArrayList<>();
+        for (int i = 0; i < MINELINES_LENGTH; i++) {
+            mineLines.add(mineLineGenerator.generate());
+        }
+        validateMineLines(mineLines);
+        this.mineLines = mineLines;
+    }
+
+    private void validateMineLines(List<MineLine> mineLines) {
+        if (countMineNumber(mineLines) != MINE_NUMBER_LIMIT) {
+            throw new IllegalArgumentException(MINELINES_LENGTH_MESSAGE);
+        }
+    }
+
+    private int countMineNumber(List<MineLine> mineLines) {
+        return (int) mineLines.stream()
+                .map(MineLine::getSquares)
+                .flatMap(square -> square.stream()
+                        .filter(Square::isHasMine))
+                .count();
+    }
+
+    public ArrayList<MineLine> getMineLines() {
+        return new ArrayList<>(mineLines);
+    }
+}
