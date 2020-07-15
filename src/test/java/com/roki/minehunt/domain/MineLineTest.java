c9f6f407d7b2e6829aff5231688c1806b26d4c58
김경록 <goodboy3421@gmail.com>
2020-07-15 22:52:57 +0900

    test: MineLine 테스트 코드 추가
    객체 생성, 길이에 대한 테스트 코드 추가

---
+++ src/test/java/com/roki/minehunt/domain/MineLineTest.java
@@ -0,0 +1,58 @@
+package com.roki.minehunt.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MineLineTest {
+
+    @DisplayName("지뢰판 한줄의 객체를 생성한다 ")
+    @Test
+    void testInitMineLine() {
+        //given
+        List<Square> squares = Arrays.asList(false, false, false, false, false,
+                false, false, false, false, false).stream()
+                .map(Square::new)
+                .collect(Collectors.toList());
+
+        //when
+        MineLine mineLine = new MineLine(squares);
+
+        //then
+        assertThat(mineLine.getSquares()).hasSize(10);
+    }
+
+    @DisplayName("지뢰판 한줄의 길이가 정해진 가로 길이보다 작으면 예외를 발생시킨다 ")
+    @Test
+    void testIfMineLineLengthLowerThenMineBoardWidthLength() {
+        //given
+        List<Square> squares = Arrays.asList(false, false, false, false, false,
+                false, false, false, false).stream()
+                .map(Square::new)
+                .collect(Collectors.toList());
+
+        //when //then
+        assertThatThrownBy(() -> new MineLine(squares))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("지뢰판 한줄의 길이가 정해진 가로 길이보다 크면 예외를 발생시킨다 ")
+    @Test
+    void testIfMineLineLengthMoreThenMineBoardWidthLength() {
+        //given
+        List<Square> squares = Arrays.asList(false, false, false, false, false,
+                false, false, false, false, false, false).stream()
+                .map(Square::new)
+                .collect(Collectors.toList());
+
+        //when //then
+        assertThatThrownBy(() -> new MineLine(squares))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+}
