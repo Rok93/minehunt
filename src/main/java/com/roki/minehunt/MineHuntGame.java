6ee8b602160dfd49e6d2ec7355cba8170445469e
김경록 <goodboy3421@gmail.com>
2020-07-16 12:50:04 +0900

    refactor: 틀린 클래스명 정정

--- src/main/java/com/roki/minehunt/MineHuntGame.java
+++ src/main/java/com/roki/minehunt/MineHuntGame.java
@@ -1,14 +1,14 @@
 package com.roki.minehunt;
 
 
-import com.roki.minehunt.domain.DefaultMineLineGenereator;
+import com.roki.minehunt.domain.DefaultMineLineGenerator;
 import com.roki.minehunt.domain.MineBoard;
 import com.roki.minehunt.domain.MineLines;
 import com.roki.minehunt.view.OutputView;
 
 public class MineHuntGame {
     public static void main(String[] args) {
-        MineLines mineLines = new MineLines(new DefaultMineLineGenereator());
+        MineLines mineLines = new MineLines(new DefaultMineLineGenerator());
         MineBoard mineBoard = new MineBoard(mineLines);
         OutputView.printMineBoard(mineBoard);
     }
---
+++ src/main/java/com/roki/minehunt/domain/DefaultMineLineGenerator.java
@@ -0,0 +1,30 @@
+package com.roki.minehunt.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DefaultMineLineGenerator implements MineLineGenerator {
+    private static final int MINEBOARD_WIDTH_LENGTH = 10;
+    private static final int MINE_LIMIT_NUMBER = 10;
+    private static final boolean NOT_MINE = false;
+
+    private int mineNumber = 0;
+
+    @Override
+    public MineLine generate() {
+        List<Square> squares = new ArrayList<>();
+        for (int i = 0; i < MINEBOARD_WIDTH_LENGTH; i++) {
+            boolean hasMine = mineNumber < MINE_LIMIT_NUMBER ? new Random().nextBoolean() : NOT_MINE;
+            countMine(hasMine);
+            squares.add(new Square(hasMine));
+        }
+        return new MineLine(squares);
+    }
+
+    private void countMine(boolean hasMine) {
+        if (hasMine) {
+            mineNumber++;
+        }
+    }
+}
