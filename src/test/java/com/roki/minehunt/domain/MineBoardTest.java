08157b4469c055e05f6cb8edb7c8f09d51268d0e
김경록 <goodboy3421@gmail.com>
2020-07-16 21:30:17 +0900

    test: 특정 사각형 출력 값을 얻는 기능의 테스트 코드 추가

--- src/test/java/com/roki/minehunt/domain/MineBoardTest.java
+++ src/test/java/com/roki/minehunt/domain/MineBoardTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MineBoardTest {
 
@@ -26,5 +27,29 @@ class MineBoardTest {
 
         //then
         assertThat(mineBoard.getMineLines()).hasSize(10);
+    }
+
+    @DisplayName("지뢰판의 특정 사각형 주변의 지뢰 갯수를 출력하는 기능을 테스트한다 ")
+    @Test
+    void testCalculateSurroundingMine() {
+        //given
+        List<Square> squares = Arrays.asList(true, false, false, false, false,
+                false, false, false, false, false).stream()
+                .map(Square::new)
+                .collect(Collectors.toList());
+        MineLines mineLines = new MineLines(() -> new MineLine(squares));
+
+        //when
+        MineBoard mineBoard = new MineBoard(mineLines);
+
+        //then
+        assertAll(
+                () -> assertThat(mineBoard.translateSquare(0, 0)).isEqualTo(" "),
+                () -> assertThat(mineBoard.translateSquare(0, 1)).isEqualTo("2"),
+                () -> assertThat(mineBoard.translateSquare(0, 2)).isEqualTo("0"),
+                () -> assertThat(mineBoard.translateSquare(1, 0)).isEqualTo(" "),
+                () -> assertThat(mineBoard.translateSquare(1, 1)).isEqualTo("3"),
+                () -> assertThat(mineBoard.translateSquare(1, 2)).isEqualTo("0")
+        );
     }
 }
