462d9e2d53b865c50fbc33213b808ebdca35b0c1
김경록 <goodboy3421@gmail.com>
2020-07-16 21:29:24 +0900

    feat: 지뢰판의 특정 사각형의 출력 값을 얻는 기능 추가
    가리키는 사각형의 지뢰 유무에 따라 숫자 혹은 공백값을 얻음

--- src/main/java/com/roki/minehunt/domain/MineBoard.java
+++ src/main/java/com/roki/minehunt/domain/MineBoard.java
@@ -1,15 +1,70 @@
 package com.roki.minehunt.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MineBoard {
+    public static final int ONE = 1;
+    public static final int ZERO = 0;
+    public static final String MINE_SYMBOL = " ";
+    public static final int FIRST_INDEX = 0;
+
     private final MineLines mineLines;
 
     public MineBoard(MineLines mineLines) {
         this.mineLines = mineLines;
     }
 
+    private List<List<Square>> getSquaresGroup() {
+        return mineLines.getMineLines().stream()
+                .map(MineLine::getSquares)
+                .collect(Collectors.toList());
+    }
+
     public List<MineLine> getMineLines() {
         return mineLines.getMineLines();
+    }
+
+    public String translateSquare(int x, int y) {
+        if (isSquareHasMine(x, y)) {
+            return MINE_SYMBOL;
+        }
+        return String.valueOf(calculateSurroundingMine(x, y));
+    }
+
+    public int calculateSurroundingMine(int x, int y) {
+        List<List<Square>> squaresGroup = getSquaresGroup();
+        int startRow = (x == FIRST_INDEX) ? x : x - 1;
+        int endRow = (x == squaresGroup.size() - 1) ? x : x + 1;
+
+        int cnt = 0;
+        for (int i = startRow; i <= endRow; i++) {
+            cnt += countColumnMine(i, y);
+        }
+        return cnt;
+    }
+
+    private int countColumnMine(int i, int y) {
+        List<List<Square>> squaresGroup = getSquaresGroup();
+        int startColumn = (y == FIRST_INDEX) ? y : y - 1;
+        int endColumn = (y == squaresGroup.get(i).size() - 1) ? y : y + 1;
+
+        int cnt = 0;
+        for (int j = startColumn; j <= endColumn; j++) {
+            cnt += addOne(i, j);
+        }
+        return cnt;
+    }
+
+    private int addOne(int i, int j) {
+        if (isSquareHasMine(i, j)) {
+            return ONE;
+        }
+        return ZERO;
+    }
+
+    private boolean isSquareHasMine(int x, int y) {
+        List<List<Square>> squaresGroup = getSquaresGroup();
+        return squaresGroup.get(x).get(y).isHasMine();
     }
 }
